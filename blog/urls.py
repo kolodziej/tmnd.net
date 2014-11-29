e34ca071b46cb744afb54b338dd5e6c0089d893e
Kacper Kołodziej <kacper@kolodziej.in>
2014-11-29 19:09:39 +0100

    added pagination

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from blog import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='blog'),
+	url(r'^(?P<page>[0-9]{1,11})$', views.index, name='blog_page'),
 	url(r'^(?P<category_slug>[0-9a-zA-Z-]+)$', views.category, name='blog_category'),
+	url(r'^(?P<category_slug>[0-9a-zA-Z-]+),(?P<page>[0-9]{1,11})$', views.category, name='blog_category_page'),
 	url(r'^(?P<post_slug>[0-9a-zA-Z-]+)\.html$', views.read, name='read_post'),
 )
--- blog/views.py
+++ blog/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.translation import ugettext as _
 from django.template import RequestContext, loader
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.models import Category, Label, Post
 
-def index(request):
+def index(request, page=1):
 	posts = Post.objects.filter(published=True)
+	paginator = Paginator(posts, 5)
 	return render(request, 'blog/index.html', {
-		'posts': posts,
+		'posts': paginator.page(page),
 	})
 
-def category(request, category_slug):
+def category(request, category_slug, page=1):
 	category = Category.objects.get(slug=category_slug)
 	posts = Post.objects.filter(category=category.id, published=True)
+	paginator = Paginator(posts, 5)
 	return render(request, 'blog/index.html', {
-		'posts': posts,
+		'posts': paginator.page(page),
 	})
 
 def read(request, post_slug):
